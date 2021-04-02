888c9f2df5a3d660824c92e03a5b78f3a1bf5e80
William <billy.ryan.rom@gmail.com>
2021-04-02 02:16:58 -0400

    Refactor Home icon on navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,12 +7,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import PhoneIcon from '@material-ui/icons/Phone';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import PersonPinIcon from '@material-ui/icons/PersonPin';
 import HelpIcon from '@material-ui/icons/Help';
 import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
-import ThumbDown from '@material-ui/icons/ThumbDown';
-import ThumbUp from '@material-ui/icons/ThumbUp';
+import HomeIcon from '@material-ui/icons/Home';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
@@ -77,7 +75,7 @@ export default function ScrollableTabsButtonForce() {
           textColor="primary"
           aria-label="scrollable force tabs example"
         >
-          <Tab label="Home" icon={<PhoneIcon />} component={Link} to="/" {...a11yProps(0)} />
+          <Tab label="Home" icon={<HomeIcon />} component={Link} to="/" {...a11yProps(0)} />
           <Tab label="Market" icon={<ShoppingBasket />} component={Link} to="/market" {...a11yProps(1)} />
           <Tab label="Profile" icon={<PersonPinIcon />} component={Link} to="/login" {...a11yProps(2)} />
           <Tab label="About Us" icon={<HelpIcon />} component={Link} to="/statement" {...a11yProps(3)} />
