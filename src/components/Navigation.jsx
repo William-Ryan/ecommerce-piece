3fb70a89af7a065337c7654a51cd37292f15907b
William <billy.ryan.rom@gmail.com>
2021-04-02 01:26:02 -0400

    Add Navigation component with base template * Add material-ui for styling

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const Navigation = () => {
+    return (
+        <div>
+            <Typography variant={"h3"}>Navigation</Typography>
+        </div>
+    )
+}
+
+export default Navigation
