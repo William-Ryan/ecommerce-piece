59b324df3c5469aa90fc6217234993e5b0a4f823
William <billy.ryan.rom@gmail.com>
2021-04-30 17:30:38 -0400

    Add input form to MarketListing.jsx with onChange/onSubmit handlers * Refactor MarketListing back to base export

--- src/components/MarketListing.jsx
+++ src/components/MarketListing.jsx
@@ -7,18 +7,87 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const MarketListing = (props) => {
+    const id = window.localStorage.getItem('id')
+
     const [marketItem, setMarketItem] = useState({
         name: '',
         category: '',
         quantity: 0,
-        seller: '',
+        seller: id,
         status: '',
         description: ''
     })
 
+    const inputHandler = e => {
+        e.preventDefault()
+        setMarketItem({ ...marketItem, [e.target.name]: e.target.value })
+    }
+
+    const submitHandler = e => {
+        e.preventDefault();
+        axiosWithAuth()
+        .post("/api/store/market", marketItem)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     return (
         <div>
-            <Typography variant={"h3"}>MarketListing Page</Typography>
+            <h1 className="title">Add your market item</h1>
+            <form onSubmit={submitHandler} className="form">
+                <input
+                    type='text'
+                    name='name'
+                    label='name'
+                    placeholder='Item Name'
+                    value={marketItem.name}
+                    onChange={inputHandler}
+                    required
+                />
+                <input
+                    type='text'
+                    name='category'
+                    label='category'
+                    placeholder='Category'
+                    value={marketItem.category}
+                    onChange={inputHandler}
+                    required
+                />
+                <input
+                    type='text'
+                    name='quantity'
+                    label='quantity'
+                    placeholder='Quantity'
+                    value={marketItem.quantity}
+                    onChange={inputHandler}
+                    required
+                />
+                <input
+                    type='text'
+                    name='status'
+                    label='status'
+                    placeholder='Status'
+                    value={marketItem.status}
+                    onChange={inputHandler}
+                    required
+                />
+                <input
+                    type='text'
+                    name='description'
+                    label='description'
+                    placeholder='Description'
+                    value={marketItem.description}
+                    onChange={inputHandler}
+                    required
+                />
+                <button className="btn" type="submit">
+                    Submit
+                </button>
+            </form>
         </div>
     )
 }
@@ -30,7 +99,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(
-    mapStateToProps,
-    { addMarketItem }
-)(MarketListing)
+export default MarketListing
