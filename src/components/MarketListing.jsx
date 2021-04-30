7470b53713b80db6442867f5e8e3ce68b4d42b9c
William <billy.ryan.rom@gmail.com>
2021-04-30 15:32:53 -0400

    Add Marketlisting.jsx for trader item listing

---
+++ src/components/MarketListing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const MarketListing = () => {
+    return (
+        <div>
+            <Typography variant={"h3"}>MarketListing Page</Typography>
+        </div>
+    )
+}
+
+export default MarketListing
