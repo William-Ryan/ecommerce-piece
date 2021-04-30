bc6283fb8d7ef607b6634221ed3b5e68eb16526a
William <billy.ryan.rom@gmail.com>
2021-04-30 15:52:44 -0400

    Add useState for market object to MarketListing.jsx * Add marketReducer.js and addMarketItem function POST request * Add axiosWithAuth.js for secure requesting for users

--- src/components/MarketListing.jsx
+++ src/components/MarketListing.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const MarketListing = () => {
+    const [marketItem, setMarketItem] = useState({
+        name: '',
+        category: '',
+        quantity: 0,
+        seller: '',
+        status: '',
+        description: ''
+    })
+
     return (
         <div>
             <Typography variant={"h3"}>MarketListing Page</Typography>
---
+++ src/redux/reducers/marketReducer.js
@@ -0,0 +1,19 @@
+import { axiosWithAuth } from  "../utils/axiosWithAuth.js"
+
+export const POST_DATA = "POST_DATA"
+export const POST_SUCCESS = "POST_SUCCESS"
+export const POST_FAIL = "POST_FAIL"
+export const addMarketItem = item => dispatch => {
+    dispatch({ type: POST_DATA })
+
+    axiosWithAuth()
+        .post(`api/store`, item)
+        .then(res => {
+            console.log(res);
+            dispatch({ type: POST_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch({ type: POST_FAIL, payload: err })
+        })
+}
