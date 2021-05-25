844ba1d2e727589665833e7defa8c85a4e00ef76
William <billy.ryan.rom@gmail.com>
2021-05-25 12:26:26 -0400

    Add fetchAllImage object into image.js for gathering all image data * Add Actions to imageReducer.js for fetchAllImage

--- src/redux/actions/image.js
+++ src/redux/actions/image.js
@@ -17,3 +17,20 @@ export const fetchImage = id => dispatch => {
             dispatch({ type: FETCH_FAIL, payload: err })
         })
 }
+
+export const FETCH_ALL_DATA = "FETCH_ALL_DATA";
+export const FETCH_ALL_SUCCESS = "FETCH_ALL_SUCCESS";
+export const FETCH_ALL_FAIL = "FETCH_ALL_FAIL";
+export const fetchAllImage = () => dispatch => {
+    
+    axios
+        .get(`http://localhost:4000/api/store/image`)
+        .then(res => {
+            console.log(res);
+            dispatch({ type: FETCH_ALL_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({ type: FETCH_ALL_FAIL, payload: err })
+        })
+}
--- src/redux/reducers/imageReducer.js
+++ src/redux/reducers/imageReducer.js
@@ -1,8 +1,11 @@
 import {
     FETCH_DATA,
     FETCH_SUCCESS,
-    FETCH_FAIL
-} from "../actions/market.js"
+    FETCH_FAIL,
+    FETCH_ALL_DATA,
+    FETCH_ALL_SUCCESS,
+    FETCH_ALL_FAIL
+} from "../actions/image.js"
 
 const initialState = {
     image: [],
@@ -26,6 +29,25 @@ const initialState = {
                 errors: ''
             }
         case FETCH_FAIL:
+            return {
+                ...state,
+                isFetching: false,
+                errors: action.payload
+            }
+        case FETCH_ALL_DATA:
+            return {
+                ...state,
+                isFetching: true,
+                errors: ''
+            }
+        case FETCH_ALL_SUCCESS:
+            return {
+                ...state,
+                image: action.payload,
+                isFetching: false,
+                errors: ''
+            }
+        case FETCH_ALL_FAIL:
             return {
                 ...state,
                 isFetching: false,
