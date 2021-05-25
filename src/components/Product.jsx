fbe6e3f7687acdcd0a8d42110b2d60f5931d7b4c
William <billy.ryan.rom@gmail.com>
2021-05-25 12:19:42 -0400

    Refactor fetchImage to redux function call

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios"
+import { connect } from "react-redux"
+
+import { fetchImage } from "../redux/actions/image.js"
 
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -8,7 +11,6 @@ import { Paper, Button } from '@material-ui/core'
 
 const Product = (props) => {
     const [product, setProduct] = useState([])
-    const [image, setImage] = useState([])
 
     const fetchProduct = id => {
         axios
@@ -21,26 +23,17 @@ const Product = (props) => {
         fetchProduct(props.match.params.id)
     }, [])
 
-    const fetchImage = id => {
-        axios
-          .get(`http://localhost:4000/api/store/image/${id}`)
-          .then(res => setImage(res.data))
-          .catch(err => console.log(err.response));
-    };
-
     useEffect(() => {
-        fetchImage(props.match.params.id)
+        props.fetchImage(props.match.params.id)
     }, [])
-
-    console.log(image.id)
 
     return (
         <div>
             <div>
-            <Typography variant={"h3"}>{product.name}</Typography>
+                <Typography variant={"h3"}>{product.name}</Typography>
             </div>
             <Carousel>
-                {image.map(photo => (
+                {props.image.map(photo => (
                     <img src={photo.image} alt="product photo"></img>
                 ))}
             </Carousel>
@@ -49,4 +42,15 @@ const Product = (props) => {
     )
 }
 
-export default Product
+const mapStateToProps = state => {
+    return {
+        image: state.image.image,
+        isFetching: state.image.isFetching,
+        errors: state.image.errors
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    { fetchImage }
+)(Product)
