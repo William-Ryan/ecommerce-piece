5bb2d52bb0b4d712ff4e88c6614a7347d125028b
William <billy.ryan.rom@gmail.com>
2021-05-27 16:06:04 -0400

    Add basic register form for user registration on UserRegister.js

--- src/components/UserRegister.jsx
+++ src/components/UserRegister.jsx
@@ -1,12 +1,74 @@
-import React from 'react';
-
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React, {useState} from "react"
+import { useHistory } from "react-router-dom"
+import axios from "axios"
 
 const UserRegister = () => {
+    const [pwd1, setPwd1] = useState(); 
+    const [signUpVol, setSignUpVol] = useState({
+        email: "",
+        password: "",
+        name: "",
+    })
+
+    const history = useHistory();
+
+    const handleChange = e => {
+        e.preventDefault();
+        setSignUpVol({
+            ...signUpVol, [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+            axios
+                .post("api/auth/volunteer/register", signUpVol)
+                .then(res => {
+                    console.log(res)
+                    history.push("/user/login")
+                })
+                .catch(err => {
+                    console.log(err.message)
+                })
+            }
+            
     return (
         <div>
-            <Typography variant={"h3"}>Register Page</Typography>
+            <div className="registerContainer">
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="name">First and Last name:</label>
+                    <input 
+                    type="text" 
+                    id="name" 
+                    name="name"
+                    value={signUpVol.name} 
+                    onChange={handleChange} 
+                    required
+                    />
+
+                    <label htmlFor="email" >Email:</label>
+                    <input 
+                    type="text" 
+                    id="email" 
+                    name="email"
+                    value={signUpVol.email} 
+                    onChange={handleChange} 
+                    required
+                    />
+
+                    <label htmlFor="password" required>Password:</label>
+                    <input 
+                    type="password"
+                    id="password"
+                    name="password" 
+                    value={signUpVol.password} 
+                    onChange={handleChange}
+                    required
+                    />
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
